e58ad373871ff961e6a0017bbefd61dacc242fb5
Haziel Ortiz <ya-yo22124@outlook.com>
2024-02-20 14:00:31 -0600

    Upd: log message

--- src/index.js
+++ src/index.js
@@ -2,5 +2,11 @@ import app from './app.js';
 import database from './databases.js';
 
 app.listen(process.env.SERVER_PORT,()=>{
-    console.log('Server on port'+process.env.SERVER_PORT);
+    console.log(`
+        ########################## 
+        \n 
+        Application running on port: ${process.env.SERVER_PORT}
+        \n
+        ##########################
+        `);
 })
